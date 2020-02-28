42458e50f06b5e6f61b4537962c50625216aea63
Danny Moulton <ddmoulton@gmail.com>
2020-02-27 19:23:51 -0500

    added code component validation

--- core/src/main/java/io/kestros/cms/components/basic/content/code/CodeComponent.java
+++ core/src/main/java/io/kestros/cms/components/basic/content/code/CodeComponent.java
@@ -19,9 +19,11 @@
 package io.kestros.cms.components.basic.content.code;
 
 import io.kestros.cms.components.basic.content.text.TextComponent;
+import io.kestros.commons.structuredslingmodels.annotation.StructuredModel;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 
+@StructuredModel(validationService = CodeComponentValidationService.class)
 @Model(adaptables = Resource.class,
        resourceType = "kestros/commons/components/content/code")
 public class CodeComponent extends TextComponent {
