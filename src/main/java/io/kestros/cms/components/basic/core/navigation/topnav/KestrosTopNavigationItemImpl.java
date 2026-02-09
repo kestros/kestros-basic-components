315051ed0a88a419c60d4f67b927422fd21c09be
Danny Moulton <danny.moulton@kestros.io>
2026-02-08 20:17:21 -0500

    added topnavigation item

---
+++ src/main/java/io/kestros/cms/components/basic/core/navigation/topnav/KestrosTopNavigationItemImpl.java
@@ -0,0 +1,83 @@
+package io.kestros.cms.components.basic.core.navigation.topnav;
+
+import io.kestros.cms.components.basic.api.content.AnchorTarget;
+import io.kestros.cms.components.basic.api.content.KestrosLink;
+import io.kestros.cms.components.basic.api.exceptions.ComponentConfigurationException;
+import io.kestros.cms.components.basic.api.navigation.KestrosTopNavigationItem;
+import io.kestros.cms.components.basic.core.BaseContainerSyntheticResource;
+import io.kestros.cms.components.basic.core.BaseSlingModelDataSource;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class KestrosTopNavigationItemImpl extends BaseContainerSyntheticResource
+    implements KestrosTopNavigationItem {
+  private KestrosLink link;
+  private List<KestrosTopNavigationItem> childItems;
+
+  public KestrosTopNavigationItemImpl(
+      @Nonnull KestrosLink link,
+      @Nonnull List<KestrosTopNavigationItem> childItems,
+      @Nonnull BaseSlingModelDataSource dataSource,
+      @Nonnull String resourcePrefix, String forcedResourceName)
+      throws ComponentConfigurationException {
+    super(dataSource, resourcePrefix, forcedResourceName);
+    this.link = link;
+    this.childItems = childItems;
+  }
+
+  @Nonnull
+  @Override
+  public List<KestrosTopNavigationItem> getNavigationItems() {
+    return new ArrayList<>(childItems);
+  }
+
+  @Nullable
+  @Override
+  public String getText() {
+    return link.getText();
+  }
+
+  @Nullable
+  @Override
+  public String getHref() {
+    return link.getHref();
+  }
+
+  @Nonnull
+  @Override
+  public AnchorTarget getTarget() {
+    return link.getTarget();
+  }
+
+  @Nullable
+  @Override
+  public String getAriaLabel() {
+    return link.getAriaLabel();
+  }
+
+  @Nullable
+  @Override
+  public String getTitle() {
+    return link.getTitle();
+  }
+
+  @Nullable
+  @Override
+  public String getRel() {
+    return link.getRel();
+  }
+
+  @Nullable
+  @Override
+  public String getAriaDescribedBy() {
+    return link.getAriaDescribedBy();
+  }
+
+  @Nullable
+  @Override
+  public String getLang() {
+    return link.getLang();
+  }
+}
