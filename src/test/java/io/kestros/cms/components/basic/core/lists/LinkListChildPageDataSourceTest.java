1aece96d002d209e47b02d63cd0828f98589ece9
Danny Moulton <danny.moulton@kestros.io>
2026-03-13 16:54:39 -0400

    fix: add None sort option to dialogs and unit tests for sort/reverse/limit
    - Added 'None (natural order)' blank-value selected option as first entry
      in all 5 sort-by selectfields (card-list child-pages, asset-list,
      tag-search; link-list child-pages, tag-search) so authors can return
      to natural JCR order after choosing a sort option
    - Added unit tests covering getSortBy default (empty string), reverse
      default (false), limit default (0), limit applied, limit zero/invalid,
      sortByTitle, sortByName, reverse, and combined sort+limit for all 3
      Java datasource classes (CardListChildPagesDataSource,
      CardListAssetsDataSource, LinkListChildPageDataSource)
    - All 218 tests pass, 0 failures

--- src/test/java/io/kestros/cms/components/basic/core/lists/LinkListChildPageDataSourceTest.java
+++ src/test/java/io/kestros/cms/components/basic/core/lists/LinkListChildPageDataSourceTest.java
@@ -66,4 +66,113 @@ public class LinkListChildPageDataSourceTest extends BaseDataSourceTest {
         linkListChildPageDataSource.getComponentUiFrameworkViewRetrievalService());
   }
 
+  @Test
+  public void testGetLinkElements_defaultSortBy_returnsNaturalOrder() {
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_sortByEmpty_returnsAll() {
+    properties.put("sortBy", "");
+    resource = context.create().resource("/content/linklist/childpages/ds-sortby-empty",
+        properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_sortByTitle() {
+    properties.put("sortBy", "title");
+    resource = context.create().resource("/content/linklist/childpages/ds-sortby-title",
+        properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_sortByName() {
+    properties.put("sortBy", "name");
+    resource = context.create().resource("/content/linklist/childpages/ds-sortby-name",
+        properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_reverseOrder() {
+    properties.put("reverse", true);
+    resource = context.create().resource("/content/linklist/childpages/ds-reverse", properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_reverseOrderDefault_false() {
+    resource = context.create().resource("/content/linklist/childpages/ds-no-reverse", properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_limitApplied() {
+    properties.put("limit", "2");
+    resource = context.create().resource("/content/linklist/childpages/ds-limit-2", properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(2, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_limitZero_returnsAll() {
+    properties.put("limit", "0");
+    resource = context.create().resource("/content/linklist/childpages/ds-limit-0", properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_limitDefault_returnsAll() {
+    resource = context.create().resource("/content/linklist/childpages/ds-no-limit", properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_limitInvalidString_returnsAll() {
+    properties.put("limit", "abc");
+    resource = context.create().resource("/content/linklist/childpages/ds-limit-invalid",
+        properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_sortByNameAndReverse() {
+    properties.put("sortBy", "name");
+    properties.put("reverse", true);
+    resource = context.create().resource("/content/linklist/childpages/ds-name-reverse",
+        properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(3, linkListChildPageDataSource.getLinkElements().size());
+  }
+
+  @Test
+  public void testGetLinkElements_sortByTitleAndLimit() {
+    properties.put("sortBy", "title");
+    properties.put("limit", "1");
+    resource = context.create().resource("/content/linklist/childpages/ds-title-limit", properties);
+    context.request().setResource(resource);
+    linkListChildPageDataSource = context.request().adaptTo(LinkListChildPageDataSource.class);
+    assertEquals(1, linkListChildPageDataSource.getLinkElements().size());
+  }
+
 }
